cef89f65a863da59c299632344581ff0f4c2775b
Mike <korostelevm@gmail.com>
2022-07-12 11:35:52 -0400

    webpack

--- demo/app.js
+++ demo/app.js
@@ -2,18 +2,6 @@ const express = require('express')
 const path = require("path");
 const app = express()
 
-// // #############################################################################
-// // Logs all request paths and method
-// app.use(function (req, res, next) {
-//   res.set('x-timestamp', Date.now())
-//   res.set('x-powered-by', 'cyclic.sh')
-//   console.log(`[${new Date().toISOString()}] ${req.ip} ${req.method} ${req.path}`);
-//   next();
-// });
-
-// // #############################################################################
-// // This configures static hosting for files in /public that have the extensions
-// // listed in the array.
 var options = {
   dotfiles: 'ignore',
   etag: false,
@@ -23,27 +11,8 @@ var options = {
   redirect: false
 }
 app.use(express.static(path.join(__dirname, 'public'), options))
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
-  });
+// app.get('/', function(req, res) {
+//     res.sendFile(path.join(__dirname, 'public/index.html'));
+//   });
 
-// #############################################################################
-// Catch all handler for all other request.
-// app.use('*', (req,res) => {
-//   res.json({
-//       at: new Date().toISOString(),
-//       method: req.method,
-//       hostname: req.hostname,
-//       ip: req.ip,
-//       query: req.query,
-//       headers: req.headers,
-//       cookies: req.cookies,
-//       params: req.params
-//     })
-//     .end()
-// })
-
-const port = process.env.PORT || 3000
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.listen(process.env.PORT || 3000)
