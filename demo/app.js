f226994d52572b9493f5fcbaad2c85740be6436f
Mike <korostelevm@gmail.com>
2022-07-12 11:33:43 -0400

    webpack

--- demo/app.js
+++ demo/app.js
@@ -14,14 +14,14 @@ const app = express()
 // // #############################################################################
 // // This configures static hosting for files in /public that have the extensions
 // // listed in the array.
-// var options = {
-//   dotfiles: 'ignore',
-//   etag: false,
-//   extensions: ['htm', 'html','css','js','ico','jpg','jpeg','png','svg'],
-//   index: ['index.html'],
-//   maxAge: '1m',
-//   redirect: false
-// }
+var options = {
+  dotfiles: 'ignore',
+  etag: false,
+  extensions: ['htm', 'html','css','js','ico','jpg','jpeg','png','svg'],
+  index: ['index.html'],
+  maxAge: '1m',
+  redirect: false
+}
 app.use(express.static(path.join(__dirname, 'public'), options))
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'public/index.html'));
