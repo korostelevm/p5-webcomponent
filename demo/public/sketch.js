99f518a6f93a63a52d94a12eba78fcab8e6183cf
Mike <korostelevm@gmail.com>
2022-07-15 13:41:04 -0400

    webpack

--- demo/public/sketch.js
+++ demo/public/sketch.js
@@ -15,40 +15,35 @@ function draw() {
   let w = 220
 
   background(36,87,218);
-  let rw = 30
-  let rh = rw * 5
+  let rw = 40 
+  let rh = rw * 5 
   
   rectMode(CENTER);
-
   
   noStroke()
   
-  
+  fill(255,255,255,150)
   push()
-  
-  // let r = rect(0, 0, rw, rh+15); 
-  translate(0,0, 70)  
   rotateY(0.4) ;
-  for (let i = 0; i< 5; i++){    
-    rotateX(0.27 * sin(l)) ;
-    rotateY(0.27 * sin(l2)) ;
-    
-    fill(255,255,255,150)
-    plane(rw,rh)
-    
+  
+    rotateX(PI)
+  
+  for (let i = 5; i>0; i--){    
+    let c = 255-255/(i+1)  
+    fill(255,255,255,c) 
+    rotateX(sin(l)/3) ;
+    rect(0, 0, rw, rh); 
   }
+  
+  
   pop()
+  
   rotate(0)
   fill(36,87,218)
   translate(0,0, 100)
-  rect(0, 0, 100, 2); 
+  rect(0, 0, 100, 1); 
   
-    
-  
-    
-
-  l2 = mouseY/200
-  l = mouseX / 600
+  l = mouseY/500
   // l = millis() / 600
 
 }
